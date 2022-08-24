ceafc13bde039df6d2901f668025d10faf2f6112
Youssef Ragab <ragab.youssef95@gmail.com>
2022-08-24 10:44:52 -0400

    fix import response bug

--- barkbnb/api/views.py
+++ barkbnb/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+
+    routes = [
+        {
+            'Endpoint': '/sittings/',
+            'method': 'POST',
+            'body': None,
+            'description':' Get all available sittings'
+        }
+    ]
+
+    return Response(routes)
