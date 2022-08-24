a977ee810ad3246f799bb0f4d06207a106ee89d4
Youssef Ragab <ragab.youssef95@gmail.com>
2022-08-24 10:16:29 -0400

    basic navbar

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Navbar } from './ui/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ frontend/src/components/ui/Navbar.jsx
@@ -0,0 +1,14 @@
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+
+export const Navbar = () => {
+  return (
+    <AppBar 
+    >
+        <Toolbar>
+            <Typography variant="h4">
+                BarkBnb
+            </Typography>
+        </Toolbar>
+    </AppBar>
+  )
+}
