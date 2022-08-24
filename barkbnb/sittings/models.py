eec792adae43149343a291fb5ffce6429ab8bc39
Youssef Ragab <ragab.youssef95@gmail.com>
2022-08-24 11:56:52 -0400

    create dog model

--- barkbnb/sittings/models.py
+++ barkbnb/sittings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from enum import unique
+from django.db.models.deletion import CASCADE
+from users.models import Profile
 
-# Create your models here.
+import uuid
+
+class Dog(models.Model):
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=CASCADE)
+    name = models.CharField(max_length=200)
+    SIZE_CHOICES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    size = models.CharField(max_length=200, choices=SIZE_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
