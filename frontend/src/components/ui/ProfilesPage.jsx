643a54b9bc8c6734b549dd5c8beebe1e0045d74b
Youssef Ragab <ragab.youssef95@gmail.com>
2022-08-26 14:54:03 -0400

    display profile image on profiles page

--- frontend/src/components/ui/ProfilesPage.jsx
+++ frontend/src/components/ui/ProfilesPage.jsx
@@ -11,29 +11,34 @@ export const ProfilesPage = () => {
 
     
     const classes = useStyles()
+    
     useEffect(() => {
         getProfiles()
     })
     
     const [profiles, setProfiles] = useState([])
 
-    let imgLink = ''
-
-    let getProfiles = async () => {
+    const [img, setImg] = useState([]);
+    
+    const getProfiles = async () => {
         let response = await fetch('http://127.0.0.1:8000/api/profiles', {
             method: "GET"
         })
         let data = await response.json()
         // console.log(data[0].profile_image)
-        imgLink = 'http://127.0.0.1:8000' + data[0].profile_image
-        console.log(imgLink)
+        let imgLink = 'http://127.0.0.1:8000' + data[0].profile_image
+        setImg(imgLink)
     }
+
+    useEffect(() => {
+    getProfiles();
+    }, [profiles])
         
 
     return (
         <div className={classes.root}>
             ProfilesPage
-            <img alt='profile' src={imgLink} />
+            <img alt='profile' src={img} />
         </div>
     )
 }
