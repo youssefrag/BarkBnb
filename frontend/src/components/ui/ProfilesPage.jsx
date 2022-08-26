bafe090231d1a2cfd1b3a408d535b9d071281b54
Youssef Ragab <ragab.youssef95@gmail.com>
2022-08-26 15:26:41 -0400

    create profiles array with image links

--- frontend/src/components/ui/ProfilesPage.jsx
+++ frontend/src/components/ui/ProfilesPage.jsx
@@ -12,33 +12,44 @@ export const ProfilesPage = () => {
     
     const classes = useStyles()
     
-    useEffect(() => {
-        getProfiles()
+    const [profile, setProfile] = useState({
+        name: '',
+        bio: '',
+        imageLink: '',
+        sitting_completed: null,
     })
-    
+
     const [profiles, setProfiles] = useState([])
 
-    const [img, setImg] = useState([]);
-    
+    useEffect(() => {
+        getProfiles();
+    }, [])
+
+
     const getProfiles = async () => {
         let response = await fetch('http://127.0.0.1:8000/api/profiles', {
             method: "GET"
         })
         let data = await response.json()
-        // console.log(data[0].profile_image)
-        let imgLink = 'http://127.0.0.1:8000' + data[0].profile_image
-        setImg(imgLink)
-    }
+        let profilesArray = []
+        for (let i = 0; i < data.length; i++) {
+            let profileObject = {}
+            profileObject.name = data[i].name
+            profileObject.bio = data[i].bio
+            profileObject.sitting_completed = data[i].sitting_completed
+            profileObject.imageLink = 'http://127.0.0.1:8000' + data[i].profile_image
+            profilesArray = profiles.push(profileObject)
+            setProfiles([profilesArray]) 
+        }
 
-    useEffect(() => {
-    getProfiles();
-    }, [profiles])
-        
+    }
+    
+    console.log(profiles)
 
     return (
         <div className={classes.root}>
             ProfilesPage
-            <img alt='profile' src={img} />
+            {/* <img alt='profile' src={img} /> */}
         </div>
     )
 }
