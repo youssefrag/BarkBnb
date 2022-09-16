7a5ab9402a6d5875d35ad22a2739dc044d539b4c
Youssef Ragab <ragab.youssef95@gmail.com>
2022-09-16 17:33:48 -0400

    hero gallery

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import heroGalleryPictures from "../assets/hero-pictures";
+
 import { Typography, Box, Container, Button } from "@mui/material";
 
 import { createStyles, makeStyles } from "@mui/styles";
@@ -10,12 +12,16 @@ const useStyles = makeStyles((theme) =>
       backgroundColor: theme.palette.primary.light3,
     },
     heroContainer: {
-      display: "flex",
       maxWidth: "130rem",
+    },
+    heroBox: {
+      paddingTop: "10rem",
+      display: "flex",
+      justifyContent: "space-between",
+      // alignItems: "center",
     },
     heroMainainInfo: {
       width: "50%",
-      paddingTop: "5.2rem",
     },
     heroBtnContainer: {
       display: "flex",
@@ -27,6 +33,33 @@ const useStyles = makeStyles((theme) =>
         border: "2px white",
       },
     },
+    heroGallery: {
+      position: "relative",
+    },
+    img1Container: {
+      position: "relative",
+      overflow: "hidden",
+      width: "40rem",
+      transform: "translate(-45rem, -20rem)",
+      position: "absolute",
+      zIndex: "1",
+    },
+    img1: {
+      transform: "translate(-30rem, 5rem)",
+    },
+    img2Container: {
+      position: "relative",
+      overflow: "hidden",
+      height: "300rem",
+      width: "500rem",
+      transform: "translate(-50rem, 25rem)",
+      position: "absolute",
+      zIndex: "2",
+    },
+    img2: {
+      transform: "scale(2,2)",
+      transform: "translate(0, 0)",
+    },
   })
 );
 
@@ -37,24 +70,40 @@ export const HomePage = () => {
     <Box className={classes.home} marginTop={8}>
       <Box className={classes.heroSection} paddingBottom={11}>
         <Container className={classes.heroContainer} maxWidth="xl">
-          <Box className={classes.heroMainainInfo}>
-            <Typography variant="h1" marginY={8}>
-              Your puppy will always be taken care of!
-            </Typography>
-            <Typography variant="h3">
-              Whether you have to go on vacation or a business trip, your puppy
-              will always be taken care of with BarkBnb!
-            </Typography>
-            <Box className={classes.heroBtnContainer} marginTop={8}>
-              <Button size="large" variant="contained">
-                Get started!
-              </Button>
-              <Button className={classes.learnMore} size="large">
-                Learn more
-              </Button>
+          <Box className={classes.heroBox}>
+            <Box className={classes.heroMainainInfo}>
+              <Typography variant="h1" marginY={8}>
+                Your puppy will always be taken care of!
+              </Typography>
+              <Typography variant="h3">
+                Whether you have to go on vacation or a business trip, your
+                puppy will always be taken care of with BarkBnb!
+              </Typography>
+              <Box className={classes.heroBtnContainer} marginTop={8}>
+                <Button size="large" variant="contained">
+                  Get started!
+                </Button>
+                <Button className={classes.learnMore} size="large">
+                  Learn more
+                </Button>
+              </Box>
+            </Box>
+            <Box className={classes.heroGallery}>
+              <Box className={classes.img1Container}>
+                <img
+                  src="https://images.unsplash.com/photo-1477884213360-7e9d7dcc1e48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGRvZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+                  className={classes.img1}
+                />
+              </Box>
+              <Box className={classes.img2Container}>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8WcE8jTid4IA_XabKaDEAaHiMFldP_ynWfg&usqp=CAU"
+                  className={classes.img2}
+                />
+                {/* */}
+              </Box>
             </Box>
           </Box>
-          <Box className={classes.heroGallery}></Box>
         </Container>
       </Box>
     </Box>
