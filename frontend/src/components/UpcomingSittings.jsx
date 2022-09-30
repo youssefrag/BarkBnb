3171ceb76cdff28991d41d8bbee40b55fa29e678
Youssef Ragab <ragab.youssef95@gmail.com>
2022-09-30 14:41:14 -0400

    build route for upcoming sittings

---
+++ frontend/src/components/UpcomingSittings.jsx
@@ -0,0 +1,10 @@
+import { useContext, useState } from "react";
+import { Button, Container } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+
+import { UserContext } from "../context/userContext";
+import { useEffect } from "react";
+
+export const UpcomingSittings = () => {
+  return <div>UpcomingSittings</div>;
+};
--- frontend/src/pages/Notifications.jsx
+++ frontend/src/pages/Notifications.jsx
@@ -5,6 +5,8 @@ import { Button, Container, Stack } from "@mui/material";
 import { OffersReceived } from "../components/OffersReceived";
 import { OffersSent } from "../components/OffersSent";
 
+import { UpcomingSittings } from "../components/UpcomingSittings";
+
 export const Notifications = () => {
   const [page, setPage] = useState("offersReceived");
 
@@ -29,7 +31,12 @@ export const Notifications = () => {
           <Button variant="contained" onClick={() => setPage("offersSent")}>
             Offers Sent
           </Button>
-          <Button variant="contained">Upcoming Sittings</Button>
+          <Button
+            variant="contained"
+            onClick={() => setPage("upcomingSittings")}
+          >
+            Upcoming Sittings
+          </Button>
         </Stack>
         <OffersReceived />;
       </Container>
@@ -44,9 +51,34 @@ export const Notifications = () => {
           <Button variant="contained" onClick={() => setPage("offersSent")}>
             Offers Sent
           </Button>
-          <Button variant="contained">Upcoming Sittings</Button>
+          <Button
+            variant="contained"
+            onClick={() => setPage("upcomingSittings")}
+          >
+            Upcoming Sittings
+          </Button>
         </Stack>
         <OffersSent />;
+      </Container>
+    );
+  } else if (page === "upcomingSittings") {
+    return (
+      <Container maxWidth="md" sx={styles.containerSx}>
+        <Stack sx={styles.stackSx}>
+          <Button variant="contained" onClick={() => setPage("offersReceived")}>
+            Offers Received
+          </Button>
+          <Button variant="contained" onClick={() => setPage("offersSent")}>
+            Offers Sent
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => setPage("upcomingSittings")}
+          >
+            Upcoming Sittings
+          </Button>
+        </Stack>
+        <UpcomingSittings />;
       </Container>
     );
   }
