0ec61d0c4bb3f03e487390efa855027ef32afb4c
Youssef Ragab <ragab.youssef95@gmail.com>
2022-10-04 15:49:16 -0400

    fix image rendering bug

--- barkbnb/frontend/src/pages/EditAccount.jsx
+++ barkbnb/frontend/src/pages/EditAccount.jsx
@@ -34,7 +34,8 @@ const useStyles = makeStyles((theme) =>
       },
     },
     imageContainer: {
-      backgroundImage: "url('/images/dalmatian.png')",
+      backgroundImage:
+        "url('https://images.unsplash.com/photo-1626435872663-b0a64169c460?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1418&q=80')",
 
       backgroundSize: "cover",
       width: "auto",
